06e810cfde14691ab1605e2b4a8321c340f6bc35
RealStorm <Patryk@MacBook-Pro-Patryk.local>
2016-10-25 15:15:17 +0200

    index is now php.
    table is working with style.

--- sites/index.php
+++ sites/index.php
@@ -1,3 +1,8 @@
+<?php 
+	include "../functions/simple_html_dom.php";
+
+	$html = file_get_html('http://livetracking.evt.com.pl/index.php/ltcontroller/getBestDriversStatsHTML');
+?>
 <!DOCTYPE html>
 <html lang="pl">
 	<head>
@@ -20,7 +25,8 @@
 				<a href="https://www.facebook.com/EuropeVirtualTransport"><img class="right" src="../assets/images/facebook.png" alt="evt-facebook"></a>
 			</header>
 			<main>
-		<aside class="left"><h3>Facebook</h3><p><iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FEuropeVirtualTransport&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="340" height="500" style="overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe></aside>
+		<aside class="left"><h3>Facebook</h3>
+		<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FEuropeVirtualTransport&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="340" height="500" style="overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe></aside>
 		<div class="middle"><h2>Rekrutacja OFF</h2><p class="welcometext">Witaj na oficjalnej stronie firmy Europe Virutal Transport.<p>
 			<p class="indextext">Firma Europe Virutal Transport powstała 29 lipca 2013r i jest jedną z najdłużej działających  wirtualnych spedycji w Polsce.
 			W naszej  pracy posługujemy się Systemem telematycznym Live Tracking System (LTS),stworzonym specjalnie na potrzeby naszej firmy.</p>
@@ -31,7 +37,11 @@
 		<aside class="right">
 			<h3>Najlepsi kierowcy</h3>
 			<table class="kierowcy">
-				<iframe src="http://livetracking.evt.com.pl/index.php/ltcontroller/getBestDriversStatsHTML" style="border: 0" "color: #9d9d9d;" width="800" height="600" frameborder="0" scrolling="no" color="#9d9d9d"></iframe>
+				<?php
+					foreach($html -> find('tr') as $element){
+						echo $element;
+					}
+				 ?>
 			</table>
 		</aside>
 		</main>
